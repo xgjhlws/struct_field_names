08bdec20e96d8cbb9f479fc9e1870e6c802bcdc8
xgjhlws <1782976935@qq.com>
2025-12-15 11:36:26 +0800

    feat: add doc for field, to fix clippy `missing_docs`; chore: update sync to 2.0, update rust edition, fix some clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-use proc_macro2::{Ident, Span};
-use quote::quote;
+use proc_macro2::Ident;
+use quote::{format_ident, quote};
 use syn::{
-    punctuated::Punctuated, token::Comma, Attribute, DeriveInput, Fields, Meta, NestedMeta,
+    parse_macro_input, punctuated::Punctuated, token::Comma, Attribute, DeriveInput, Fields,
     Variant, Visibility,
 };
 
 #[proc_macro_derive(StructFieldNames, attributes(struct_field_names))]
 pub fn derive_field_names(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast: DeriveInput = syn::parse(input).unwrap();
-    let (vis, ty, generics) = (&ast.vis, &ast.ident, &ast.generics);
-    let names_struct_ident = Ident::new(&(ty.to_string() + "FieldStaticStr"), Span::call_site());
+    let ast = parse_macro_input!(input as DeriveInput);
+    let (vis, ident, generics) = (&ast.vis, &ast.ident, &ast.generics);
+    let names_struct_ident = format_ident!("{}FieldStaticStr", ident.to_string());
 
     let fields = filter_fields(match ast.data {
         syn::Data::Struct(ref s) => &s.fields,
@@ -24,7 +24,9 @@ pub fn derive_field_names(input: proc_macro::TokenStream) -> proc_macro::TokenSt
 
     let names_const_fields = fields.iter().map(|(_vis, ident)| {
         let ident_name = ident.to_string();
+        let doc_comment = format!("field name for `{ident_name}`");
         quote! {
+            #[doc = #doc_comment]
             #ident: #ident_name
         }
     });
@@ -36,9 +38,10 @@ pub fn derive_field_names(input: proc_macro::TokenStream) -> proc_macro::TokenSt
             #(#names_struct_fields),*
         }
 
-        impl #impl_generics #ty #ty_generics
+        impl #impl_generics #ident #ty_generics
             #where_clause
         {
+
             #vis const FIELD_NAMES: #names_struct_ident = #names_struct_ident {
                 #(#names_const_fields),*
             };
@@ -49,9 +52,9 @@ pub fn derive_field_names(input: proc_macro::TokenStream) -> proc_macro::TokenSt
 
 #[proc_macro_derive(EnumVariantNames, attributes(enum_variant_names))]
 pub fn derive_variant_names(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast: DeriveInput = syn::parse(input).unwrap();
-    let (vis, ty, generics) = (&ast.vis, &ast.ident, &ast.generics);
-    let names_struct_ident = Ident::new(&(ty.to_string() + "VariantsStaticStr"), Span::call_site());
+    let ast = parse_macro_input!(input as DeriveInput);
+    let (vis, ident, generics) = (&ast.vis, &ast.ident, &ast.generics);
+    let names_struct_ident = format_ident!("{}VariantsStaticStr", ident.to_string());
 
     let variants = filter_variants(match ast.data {
         syn::Data::Enum(ref e) => &e.variants,
@@ -79,7 +82,7 @@ pub fn derive_variant_names(input: proc_macro::TokenStream) -> proc_macro::Token
             #(#names_struct_fields),*
         }
 
-        impl #impl_generics #ty #ty_generics
+        impl #impl_generics #ident #ty_generics
             #where_clause
         {
             #vis const VARIANT_NAMES: #names_struct_ident = #names_struct_ident {
@@ -94,11 +97,10 @@ fn filter_fields(fields: &Fields) -> Vec<(Visibility, Ident)> {
     fields
         .iter()
         .filter_map(|field| {
-            if field
+            if !field
                 .attrs
                 .iter()
-                .find(|attr| has_skip_attr(attr, "struct_field_names"))
-                .is_none()
+                .any(|attr| has_skip_attr(attr, "struct_field_names"))
                 && field.ident.is_some()
             {
                 let field_vis = field.vis.clone();
@@ -115,11 +117,10 @@ fn filter_variants(variants: &Punctuated<Variant, Comma>) -> Vec<Ident> {
     variants
         .iter()
         .filter_map(|variant| {
-            if variant
+            if !variant
                 .attrs
                 .iter()
-                .find(|attr| has_skip_attr(attr, "enum_variant_names"))
-                .is_none()
+                .any(|attr| has_skip_attr(attr, "enum_variant_names"))
             {
                 let ident = variant.ident.clone();
                 Some(ident)
@@ -130,19 +131,20 @@ fn filter_variants(variants: &Punctuated<Variant, Comma>) -> Vec<Ident> {
         .collect::<Vec<_>>()
 }
 
-const ATTR_META_SKIP: &'static str = "skip";
+const ATTR_META_SKIP: &str = "skip";
 
 fn has_skip_attr(attr: &Attribute, path: &'static str) -> bool {
-    if let Ok(Meta::List(meta_list)) = attr.parse_meta() {
-        if meta_list.path.is_ident(path) {
-            for nested_item in meta_list.nested.iter() {
-                if let NestedMeta::Meta(Meta::Path(path)) = nested_item {
-                    if path.is_ident(ATTR_META_SKIP) {
-                        return true;
-                    }
-                }
-            }
-        }
+    if !attr.path().is_ident(path) {
+        return false;
     }
-    false
+
+    let mut has_skip = false;
+    let result = attr.parse_nested_meta(|meta| {
+        if meta.path.is_ident(ATTR_META_SKIP) {
+            has_skip = true;
+        }
+        Ok(())
+    });
+
+    result.is_ok() && has_skip
 }
